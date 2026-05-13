e7880ff8dc77e315857ae688893064b86bfd017f
otokodev <mr.nice.otoko@gmail.com>
2026-05-13 15:11:26 +0800

    change folder
    updated

--- demopage/src/app/demopage/demopage.page.ts
+++ demopage/src/app/demopage/demopage.page.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonRow, IonSearchbar, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-demopage',
   templateUrl: './demopage.page.html',
   styleUrls: ['./demopage.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonText,IonSearchbar,IonButton,IonCol,IonRow,IonGrid,IonText,IonCardContent,IonCard,IonIcon, CommonModule,FormsModule]
 })
 export class DemopagePage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-}
+  }}
