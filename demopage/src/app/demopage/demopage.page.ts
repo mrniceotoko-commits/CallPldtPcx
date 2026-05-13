e4e9efbc7107e36469da66b6449e774085adcb8c
otokodev <mr.nice.otoko@gmail.com>
2026-05-13 16:54:07 +0800

    update
    fix

--- demopage/src/app/demopage/demopage.page.ts
+++ demopage/src/app/demopage/demopage.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonRow, IonSearchbar, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonCard, IonCardContent, IonContent,IonToolbar, IonGrid, IonHeader, IonIcon, IonRow, IonSearchbar, IonText, IonTitle, IonCol } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { refreshOutline } from 'ionicons/icons';    
+
 
 @Component({
   selector: 'app-demopage',
   templateUrl: './demopage.page.html',
   styleUrls: ['./demopage.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonText,IonSearchbar,IonButton,IonCol,IonRow,IonGrid,IonText,IonCardContent,IonCard,IonIcon, CommonModule,FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonText,IonSearchbar,IonButton,IonGrid,IonCol,IonRow,IonText,IonCardContent,IonCard,IonIcon, CommonModule,FormsModule]
 })
 export class DemopagePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons
+    ({
+      'refresh-outline': refreshOutline
+    });
+   }
 
   ngOnInit() {
   }}
